4c52f76df810e0f394ab65bda7bb462c32fe11c2
danilo <dbonilla@shoppingscout.com>
2014-09-01 16:48:27 -0700

    Updates to descriptive statistic element, formerly Numeric Descriptor.

---
+++ src/main/java/org/openmhealth/schema/pojos/generic/DescriptiveStatistic.java
@@ -0,0 +1,5 @@
+package org.openmhealth.schema.pojos.generic;
+
+public enum DescriptiveStatistic {
+    average, maximum, minimum, standard_deviation, variance, sum, median
+}
--- src/main/java/org/openmhealth/schema/pojos/generic/NumericDescriptor.java
+++
@@ -1,5 +0,0 @@
-package org.openmhealth.schema.pojos.generic;
-
-public enum NumericDescriptor {
-    average, maximum, minimum, standard_deviation
-}
