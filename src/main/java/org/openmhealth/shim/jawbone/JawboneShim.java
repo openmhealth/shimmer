ed54654c7bbaef24e0d63d590aa7970067ae66f3
Emerson Farrugia <emerson.farrugia@gmail.com>
2014-09-08 13:39:00 +0200

    Add missing Apache License headers.

--- src/main/java/org/openmhealth/shim/jawbone/JawboneShim.java
+++ src/main/java/org/openmhealth/shim/jawbone/JawboneShim.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openmhealth.shim.jawbone;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -37,7 +53,9 @@ import static org.openmhealth.schema.pojos.generic.DurationUnitValue.DurationUni
 import static org.openmhealth.schema.pojos.generic.LengthUnitValue.LengthUnit;
 
 /**
- * Encapsulates parameters specific to jawbone api.
+ * Encapsulates parameters specific to the Jawbone API.
+ *
+ * @author Danilo Bonilla
  */
 public class JawboneShim extends OAuth2ShimBase {
 
--- src/test/java/org/openmhealth/schema/pojos/build/BodyHeightBuilderTest.java
+++ src/test/java/org/openmhealth/schema/pojos/build/BodyHeightBuilderTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openmhealth.schema.pojos.build;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -9,8 +25,6 @@ import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.openmhealth.schema.pojos.BodyHeight;
-import org.openmhealth.schema.pojos.BodyWeight;
-import org.openmhealth.schema.pojos.generic.LengthUnitValue;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,9 +32,7 @@ import java.math.BigDecimal;
 import java.net.URL;
 
 import static org.junit.Assert.*;
-import static org.openmhealth.schema.pojos.generic.LengthUnitValue.*;
-import static org.openmhealth.schema.pojos.generic.LengthUnitValue.LengthUnit.*;
-import static org.openmhealth.schema.pojos.generic.MassUnitValue.MassUnit.lb;
+import static org.openmhealth.schema.pojos.generic.LengthUnitValue.LengthUnit.cm;
 
 /**
  * @author Danilo Bonilla
@@ -70,5 +82,4 @@ public class BodyHeightBuilderTest {
 
         assertTrue("Expected valid result!", report.isSuccess());
     }
-
 }
