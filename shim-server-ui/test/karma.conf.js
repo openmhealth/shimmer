47a0f185eab75838d82f9c69200495c9a16a5269
danilo <dbonilla@shoppingscout.com>
2014-09-13 12:11:36 -0700

    Corrections and additions to gitignore.

--- shim-server-ui/test/karma.conf.js
+++ shim-server-ui/test/karma.conf.js
@@ -43,13 +43,13 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
 
     colors: true,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
