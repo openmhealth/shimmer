488442450ffde135beb8c9008960018cb9c5b319
Eric Jain <ejain@zenobase.com>
2015-04-16 12:39:09 -0700

    Refactor shim "normalizer" tests (and add some missing tests)

--- shim-server/src/test/java/org/openmhealth/shim/googlefit/GoogleFitShimTest.java
+++ shim-server/src/test/java/org/openmhealth/shim/googlefit/GoogleFitShimTest.java
@@ -1,123 +1,75 @@
-/*
- * Copyright 2014 Open mHealth
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.openmhealth.shim.googlefit;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import org.joda.time.DateTime;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.openmhealth.schema.pojos.Activity;
 import org.openmhealth.schema.pojos.BodyHeight;
 import org.openmhealth.schema.pojos.BodyWeight;
-import org.openmhealth.schema.pojos.DataPoint;
 import org.openmhealth.schema.pojos.HeartRate;
 import org.openmhealth.schema.pojos.HeartRateUnitValue;
 import org.openmhealth.schema.pojos.StepCount;
 import org.openmhealth.schema.pojos.generic.LengthUnitValue.LengthUnit;
 import org.openmhealth.schema.pojos.generic.MassUnitValue.MassUnit;
-import org.openmhealth.shim.ShimDataResponse;
-import org.openmhealth.shim.googlefit.GoogleFitShim.GoogleFitDataTypes;
+import org.openmhealth.shim.testing.ShimTestSupport;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigDecimal;
-import java.net.URL;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-/**
- * @author Eric Jain
- */
-public class GoogleFitShimTest {
+public class GoogleFitShimTest extends ShimTestSupport {
 
 	@Test
-    public void testActivity() throws IOException {
-        List<Activity> dataPoints = getDataPoints("googlefit-activity.json", GoogleFitShim.GoogleFitDataTypes.ACTIVITY);
-        assertTrue(dataPoints.size() == 3);
-        assertEquals(DateTime.parse("2015-01-01T22:21:57.000Z"), dataPoints.get(0).getEffectiveTimeFrame().getTimeInterval().getStartTime());
-        assertEquals(DateTime.parse("2015-01-01T23:29:49.000Z"), dataPoints.get(0).getEffectiveTimeFrame().getTimeInterval().getEndTime());
-        assertEquals("Walking", dataPoints.get(0).getActivityName());
+    public void testActivity() {
+
+	    List<Activity> datapoints = read("googlefit-activity.json", Activity.SCHEMA_ACTIVITY, GoogleFitShim.GoogleFitDataTypes.ACTIVITY.getNormalizer());
+        assertEquals(3, datapoints.size());
+        
+        assertTimeFrameEquals("2015-01-01T22:21:57.000Z", "2015-01-01T23:29:49.000Z", datapoints.get(0).getEffectiveTimeFrame());
+        assertEquals("Walking", datapoints.get(0).getActivityName());
     }
 
 	@Test
-    public void testBodyHeight() throws IOException {
-        List<BodyHeight> dataPoints = getDataPoints("googlefit-body-height.json", GoogleFitShim.GoogleFitDataTypes.BODY_HEIGHT);
-        assertTrue(dataPoints.size() == 3);
-        assertEquals(DateTime.parse("2014-10-28T20:30:22.587Z"), dataPoints.get(0).getEffectiveTimeFrame().getDateTime());
-        assertEquals(new BigDecimal("1.850000023841858"), dataPoints.get(0).getLengthUnitValue().getValue());
-        assertEquals(LengthUnit.m, dataPoints.get(0).getLengthUnitValue().getUnit());
+    public void testBodyHeight() {
+
+	    List<BodyHeight> datapoints = read("googlefit-body-height.json", BodyHeight.SCHEMA_BODY_HEIGHT, GoogleFitShim.GoogleFitDataTypes.BODY_HEIGHT.getNormalizer());
+        assertEquals(3, datapoints.size());
+        
+        assertTimeFrameEquals("2014-10-28T20:30:22.587Z", datapoints.get(0).getEffectiveTimeFrame());
+        assertLengthUnitEquals("1.850000023841858", LengthUnit.m, datapoints.get(0).getLengthUnitValue());
     }
 
 	@Test
-    public void testBodyWeight() throws IOException {
-        List<BodyWeight> dataPoints = getDataPoints("googlefit-body-weight.json", GoogleFitShim.GoogleFitDataTypes.BODY_WEIGHT);
-        assertTrue(dataPoints.size() == 3);
-        assertEquals(DateTime.parse("2015-02-13T00:00:00.000Z"), dataPoints.get(0).getEffectiveTimeFrame().getDateTime());
-        assertEquals(new BigDecimal("72.0999984741211"), dataPoints.get(0).getMassUnitValue().getValue());
-        assertEquals(MassUnit.kg, dataPoints.get(0).getMassUnitValue().getUnit());
+    public void testBodyWeight() {
+        
+	    List<BodyWeight> datapoints = read("googlefit-body-weight.json", BodyWeight.SCHEMA_BODY_WEIGHT, GoogleFitShim.GoogleFitDataTypes.BODY_WEIGHT.getNormalizer());
+        assertEquals(3, datapoints.size());
+
+        assertTimeFrameEquals("2015-02-13T00:00:00.000Z", datapoints.get(0).getEffectiveTimeFrame());
+        assertMassUnitEquals("72.0999984741211", MassUnit.kg, datapoints.get(0).getMassUnitValue());
     }
 
 	@Test
-    public void testHeartRate() throws IOException {
-        List<HeartRate> dataPoints = getDataPoints("googlefit-heart-rate.json", GoogleFitShim.GoogleFitDataTypes.HEART_RATE);
-        assertTrue(dataPoints.size() == 3);
-        assertEquals(DateTime.parse("2015-01-30T15:37:48.186Z"), dataPoints.get(0).getEffectiveTimeFrame().getDateTime());
-        assertEquals(Integer.valueOf(54), dataPoints.get(0).getHeartRate().getValue());
-        assertEquals(HeartRateUnitValue.Unit.bpm, dataPoints.get(0).getHeartRate().getUnit());
+    public void testHeartRate() {
+
+	    List<HeartRate> datapoints = read("googlefit-heart-rate.json", HeartRate.SCHEMA_HEART_RATE, GoogleFitShim.GoogleFitDataTypes.HEART_RATE.getNormalizer());
+        assertEquals(3, datapoints.size());
+        
+        assertTimeFrameEquals("2015-01-30T15:37:48.186Z", datapoints.get(0).getEffectiveTimeFrame());
+        assertHeartRateUnitEquals(54, HeartRateUnitValue.Unit.bpm, datapoints.get(0).getHeartRate());
     }
 
 	@Test
-    public void testStepCount() throws IOException {
-        List<StepCount> dataPoints = getDataPoints("googlefit-step-count.json", GoogleFitShim.GoogleFitDataTypes.STEP_COUNT);
-        assertTrue(dataPoints.size() == 1);
-        assertEquals(DateTime.parse("2015-02-02T22:49:39.811Z"), dataPoints.get(0).getEffectiveTimeFrame().getTimeInterval().getStartTime());
-        assertEquals(DateTime.parse("2015-02-02T23:25:20.811Z"), dataPoints.get(0).getEffectiveTimeFrame().getTimeInterval().getEndTime());
-        assertEquals(Integer.valueOf(4146), dataPoints.get(0).getStepCount());
+    public void testStepCount() {
+        
+	    List<StepCount> datapoints = read("googlefit-step-count.json", StepCount.SCHEMA_STEP_COUNT, GoogleFitShim.GoogleFitDataTypes.STEP_COUNT.getNormalizer());
+        assertEquals(1, datapoints.size());
+        
+        assertTimeFrameEquals("2015-02-02T22:49:39.811Z", "2015-02-02T23:25:20.811Z", datapoints.get(0).getEffectiveTimeFrame());
+        assertEquals(Integer.valueOf(4146), datapoints.get(0).getStepCount());
     }
 
 	@Test
-    public void testEmpty() throws IOException {
-        List<StepCount> dataPoints = getDataPoints("googlefit-empty.json", GoogleFitShim.GoogleFitDataTypes.STEP_COUNT);
-        assertTrue(dataPoints == null);
+    public void testEmpty() {
+        List<StepCount> datapoints = read("googlefit-empty.json", StepCount.SCHEMA_STEP_COUNT, GoogleFitShim.GoogleFitDataTypes.STEP_COUNT.getNormalizer());
+        assertNull(datapoints);
     }
-
-    @SuppressWarnings("unchecked")
-	private static <T extends DataPoint> List<T> getDataPoints(String resource, GoogleFitDataTypes type) throws IOException {
-
-        URL url = Thread.currentThread().getContextClassLoader().getResource(resource);
-        InputStream inputStream = url.openStream();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(ShimDataResponse.class, type.getNormalizer());
-
-        objectMapper.registerModule(module);
-
-        ShimDataResponse response = objectMapper.readValue(inputStream, ShimDataResponse.class);
-
-        assertNotNull(response);
-        assertNotNull(response.getShim());
-
-		Map<String, List<T>> map = (Map<String, List<T>>) response.getBody();
-        return map != null ? map.get(type.getSchemaId()) : null;
-	}
 }
--- shim-server/src/test/java/org/openmhealth/shim/misfit/MisfitShimTest.java
+++ shim-server/src/test/java/org/openmhealth/shim/misfit/MisfitShimTest.java
@@ -1,85 +1,48 @@
 package org.openmhealth.shim.misfit;
 
+import static org.junit.Assert.assertEquals;
 
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.google.common.io.Resources;
-import org.joda.time.DateTime;
+import java.util.List;
+
 import org.junit.Test;
 import org.openmhealth.schema.pojos.Activity;
-import org.openmhealth.schema.pojos.DataPoint;
 import org.openmhealth.schema.pojos.SleepDuration;
 import org.openmhealth.schema.pojos.SleepDurationUnitValue;
 import org.openmhealth.schema.pojos.StepCount;
 import org.openmhealth.schema.pojos.generic.DurationUnitValue.DurationUnit;
 import org.openmhealth.schema.pojos.generic.LengthUnitValue.LengthUnit;
-import org.openmhealth.shim.ShimDataResponse;
+import org.openmhealth.shim.testing.ShimTestSupport;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.URL;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-public class MisfitShimTest {
+public class MisfitShimTest extends ShimTestSupport {
 
     @Test
     public void testMoves() {
-        ShimDataResponse response = read("misfit-moves.json", MisfitShim.MisfitDataTypes.MOVES.getNormalizer());
-        assertEquals("misfit", response.getShim());
-        List<StepCount> stepCounts = get(StepCount.SCHEMA_STEP_COUNT, response);
-        assertTrue(stepCounts.size() == 3);
-        assertEquals(DateTime.parse("2015-04-13T00:00:00Z"), stepCounts.get(0).getEffectiveTimeFrame().getTimeInterval().getStartTime());
-        assertEquals(DateTime.parse("2015-04-14T00:00:00Z"), stepCounts.get(0).getEffectiveTimeFrame().getTimeInterval().getEndTime());
-        assertEquals(Integer.valueOf(26370), stepCounts.get(0).getStepCount());
+        
+        List<StepCount> datapoints = read("misfit-moves.json", StepCount.SCHEMA_STEP_COUNT, MisfitShim.MisfitDataTypes.MOVES.getNormalizer());
+        assertEquals(3, datapoints.size());
+
+        assertTimeFrameEquals("2015-04-13T00:00:00Z", "2015-04-14T00:00:00Z", datapoints.get(0).getEffectiveTimeFrame());
+        assertEquals(Integer.valueOf(26370), datapoints.get(0).getStepCount());
     }
 
     @Test
     public void testActivities() {
-        ShimDataResponse response = read("misfit-activities.json", MisfitShim.MisfitDataTypes.ACTIVITIES.getNormalizer());
-        assertEquals("misfit", response.getShim());
-        List<Activity> activities = get(Activity.SCHEMA_ACTIVITY, response);
-        assertTrue(activities.size() == 3);
-        assertEquals("Walking", activities.get(0).getActivityName());
-        assertEquals(DateTime.parse("2015-04-13T11:46:00-07:00"), activities.get(0).getEffectiveTimeFrame().getTimeInterval().getStartTime());
-        assertEquals(BigDecimal.valueOf(1140), activities.get(0).getEffectiveTimeFrame().getTimeInterval().getDuration().getValue());
-        assertEquals(DurationUnit.sec, activities.get(0).getEffectiveTimeFrame().getTimeInterval().getDuration().getUnit());
-        assertEquals(new BigDecimal("0.9371"), activities.get(0).getDistance().getValue());
-        assertEquals(LengthUnit.km, activities.get(0).getDistance().getUnit());
+        
+        List<Activity> datapoints = read("misfit-activities.json", Activity.SCHEMA_ACTIVITY, MisfitShim.MisfitDataTypes.ACTIVITIES.getNormalizer());
+        assertEquals(3, datapoints.size());
+        
+        assertTimeFrameEquals("2015-04-13T11:46:00-07:00", 1140, DurationUnit.sec, datapoints.get(0).getEffectiveTimeFrame());
+        assertEquals("Walking", datapoints.get(0).getActivityName());
+        assertLengthUnitEquals("0.9371", LengthUnit.km, datapoints.get(0).getDistance());
     }
 
     @Test
     public void testSleep() {
-        ShimDataResponse response = read("misfit-sleep.json", MisfitShim.MisfitDataTypes.SLEEP.getNormalizer());
-        assertEquals("misfit", response.getShim());
-        List<SleepDuration> sleepDurations = get(SleepDuration.SCHEMA_SLEEP_DURATION, response);
-        assertTrue(sleepDurations.size() == 1);
-        assertEquals(DateTime.parse("2015-04-14T21:35:05-07:00"), sleepDurations.get(0).getEffectiveTimeFrame().getTimestamp());
-        assertEquals(BigDecimal.valueOf(215), sleepDurations.get(0).getSleepDurationUnitValue().getValue());
-        assertEquals(SleepDurationUnitValue.Unit.min, sleepDurations.get(0).getSleepDurationUnitValue().getUnit());
-    }
-
-    private static ShimDataResponse read(String resource, JsonDeserializer<ShimDataResponse> deserializer) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            SimpleModule module = new SimpleModule();
-            module.addDeserializer(ShimDataResponse.class, deserializer);
-            objectMapper.registerModule(module);
-            URL url = Resources.getResource(resource);
-            return objectMapper.readValue(url.openStream(), ShimDataResponse.class);
-        } catch (IOException e) {
-            throw new AssertionError(e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T extends DataPoint> List<T> get(String schema, ShimDataResponse response) {
-        Map<String, List<T>> map = (Map<String, List<T>>) response.getBody();
-        assertTrue(map.containsKey(schema));
-        return map.get(schema);
+        
+        List<SleepDuration> datapoints = read("misfit-sleep.json", SleepDuration.SCHEMA_SLEEP_DURATION, MisfitShim.MisfitDataTypes.SLEEP.getNormalizer());
+        assertEquals(1, datapoints.size());
+        
+        assertTimeFrameEquals("2015-04-14T21:35:05-07:00", datapoints.get(0).getEffectiveTimeFrame());
+        assertSleepDurationUnitEquals(215, SleepDurationUnitValue.Unit.min, datapoints.get(0).getSleepDurationUnitValue());
     }
 }
