f3cf0f56c2d240f6be62ef9c2ac2a3d266158d05
Chris Schaefbauer <schaefba@colorado.edu>
2015-06-23 14:33:02 -0600

    Fix parent class for Fitbit step count mapper

--- shim-server/src/main/java/org/openmhealth/shim/fitbit/mapper/FitbitStepCountDataPointMapper.java
+++ shim-server/src/main/java/org/openmhealth/shim/fitbit/mapper/FitbitStepCountDataPointMapper.java
@@ -14,21 +14,17 @@ import static org.openmhealth.shim.common.mapper.JsonNodeMappingSupport.*;
 /**
  * @author Chris Schaefbauer
  */
-public class FitbitStepCountDataPointMapper extends FitbitDataPointMapper {
+public class FitbitStepCountDataPointMapper extends FitbitDataPointMapper<StepCount> {
     @Override
-    protected Optional<DataPoint> asDataPoint(JsonNode node, int offsetInMilliseconds) {
+    protected Optional<DataPoint<StepCount>> asDataPoint(JsonNode node, int offsetInMilliseconds) {
 
         StepCount.Builder builder = new StepCount.Builder(Integer.parseInt(asRequiredString(node,"value")));
 
-        //LocalDate stepDate = LocalDate.parse(node.get("dateTime").asText(),DateTimeFormatter.ISO_LOCAL_DATE);
         Optional<LocalDate> stepDate = asOptionalLocalDate(node, "dateTime");
-
         if(stepDate.isPresent()){
             LocalDateTime startDateTime = stepDate.get().atTime(0,0,0,0);
             LocalDateTime endDateTime = stepDate.get().atTime(23,59,59,999999999);
 
-//            builder.setEffectiveTimeFrame(TimeInterval.ofStartDateTimeAndEndDateTime(combineDateTimeAndTimezone(startTime.get().withHour(0).withMinute(0).withSecond(0).withNano(0), offsetInMilliseconds),
-//                    combineDateTimeAndTimezone(endTime.get().withHour(23).withMinute(59).withSecond(59).withNano(999999999), offsetInMilliseconds)));
             builder.setEffectiveTimeFrame(TimeInterval.ofStartDateTimeAndEndDateTime(combineDateTimeAndTimezone(startDateTime, offsetInMilliseconds),
                     combineDateTimeAndTimezone(endDateTime, offsetInMilliseconds)));
 
