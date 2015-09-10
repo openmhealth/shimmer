b09c5ad0826577bd28d89df41aada0b27fd8450f
Emerson Farrugia <emerson@openmhealth.org>
2015-09-10 15:22:53 +0200

    Extract the script to update Docker Compose files

--- run-dockerized.sh
+++ run-dockerized.sh
@@ -13,22 +13,14 @@ then
     exit 1
 fi
 
-if ! hash "docker-machine" 2>/dev/null;
-then
-    echo "docker-machine can't be found"
-    exit 1
-fi
-
-if [[ -z "$DOCKER_MACHINE_NAME" ]]; then
-    echo "DOCKER_MACHINE_NAME environment variable isn't set. Have you run docker-machine env?"
-    exit 1
-fi
-
 if ! hash "docker-compose" 2>/dev/null;
 then
     echo "docker-compose can't be found"
     exit 1
 fi
+
+# check for Docker Compose tooling and update configuration
+. update-compose-files.sh
 
 # build the console
 echo -n "Do you want to rebuild the console (y/N)? "
@@ -72,12 +64,6 @@ if echo "$answer" | grep -iq "^y" ;then
 fi
 
 # run the containers
-ip=$(docker-machine ip $DOCKER_MACHINE_NAME)
-echo Updating the Docker Compose configuration to match your environment...
-cd ${BASEDIR} #CMD
-sed -i ".bak" -e "s,.*OPENMHEALTH_SHIM_SERVER_CALLBACKURLBASE.*,    OPENMHEALTH_SHIM_SERVER_CALLBACKURLBASE: http://${ip}:8083,g" docker-compose-build.yml
-#CMD modify the URL in docker-compose-build.yml to point to the IP of your Docker host
-
 echo Building the containers...
 docker-compose -f docker-compose-build.yml build #CMD
 
