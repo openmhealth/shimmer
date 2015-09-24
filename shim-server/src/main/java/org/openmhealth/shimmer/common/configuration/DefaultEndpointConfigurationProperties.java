590de57161898dfc4ba9040c6534e20e3b215629
Emerson Farrugia <emerson@openmhealth.org>
2015-09-24 22:36:41 +0200

    Implement first batch of date time range classes

---
+++ shim-server/src/main/java/org/openmhealth/shimmer/common/configuration/DefaultEndpointConfigurationProperties.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openmhealth.shimmer.common.configuration;
+
+import org.springframework.web.util.UriTemplate;
+
+import java.util.Optional;
+
+
+/**
+ * @author Emerson Farrugia
+ */
+public class DefaultEndpointConfigurationProperties implements EndpointConfigurationProperties {
+
+    private String id;
+    private UriTemplate uriTemplate;
+    private DateTimeQueryConfigurationProperties effectiveDateTimeQuerySettings;
+    private DateTimeQueryConfigurationProperties creationDateTimeQuerySettings;
+    private DateTimeQueryConfigurationProperties modificationDateTimeQuerySettings;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public UriTemplate getUriTemplate() {
+        return uriTemplate;
+    }
+
+    public void setUriTemplate(UriTemplate uriTemplate) {
+        this.uriTemplate = uriTemplate;
+    }
+
+    @Override
+    public Optional<DateTimeQueryConfigurationProperties> getEffectiveDateTimeQuerySettings() {
+        return Optional.ofNullable(effectiveDateTimeQuerySettings);
+    }
+
+    public void setEffectiveDateTimeQuerySettings(DateTimeQueryConfigurationProperties settings) {
+        this.effectiveDateTimeQuerySettings = settings;
+    }
+
+    @Override
+    public Optional<DateTimeQueryConfigurationProperties> getCreationDateTimeQuerySettings() {
+        return Optional.ofNullable(creationDateTimeQuerySettings);
+    }
+
+    public void setCreationDateTimeQuerySettings(DateTimeQueryConfigurationProperties settings) {
+        this.creationDateTimeQuerySettings = settings;
+    }
+
+    @Override
+    public Optional<DateTimeQueryConfigurationProperties> getModificationDateTimeQuerySettings() {
+        return Optional.ofNullable(modificationDateTimeQuerySettings);
+    }
+
+    public void setModificationDateTimeQuerySettings(DateTimeQueryConfigurationProperties settings) {
+        this.modificationDateTimeQuerySettings = settings;
+    }
+}
---
+++ shim-server/src/main/java/org/openmhealth/shimmer/common/configuration/EndpointConfigurationProperties.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openmhealth.shimmer.common.configuration;
+
+import org.springframework.web.util.UriTemplate;
+
+import java.util.Optional;
+
+
+/**
+ * @author Emerson Farrugia
+ */
+public interface EndpointConfigurationProperties {
+
+    /**
+     * @return the identifier of this endpoint
+     */
+    String getId();
+
+    /**
+     * @return the URI template used to send HTTP requests to this endpoint
+     */
+    UriTemplate getUriTemplate();
+
+    /**
+     * @return the settings used by this endpoint to handle creation date time range queries
+     */
+    Optional<DateTimeQueryConfigurationProperties> getCreationDateTimeQuerySettings();
+
+    /**
+     * @return true if this endpoint supports creation data time range queries
+     */
+    default boolean supportsCreationDateTimeQueries() {
+        return getCreationDateTimeQuerySettings().isPresent();
+    }
+
+    /**
+     * @return the settings used by this endpoint to handle effective date time range queries
+     */
+    Optional<DateTimeQueryConfigurationProperties> getEffectiveDateTimeQuerySettings();
+
+    /**
+     * @return true if this endpoint supports effective data time range queries
+     */
+    default boolean supportsEffectiveDateTimeQueries() {
+        return getEffectiveDateTimeQuerySettings().isPresent();
+    }
+
+    /**
+     * @return the settings used by this endpoint to handle modification date time range queries
+     */
+    Optional<DateTimeQueryConfigurationProperties> getModificationDateTimeQuerySettings();
+
+    /**
+     * @return true if this endpoint supports modification data time range queries
+     */
+    default boolean supportsModificationDateTimeQueries() {
+        return getModificationDateTimeQuerySettings().isPresent();
+    }
+}
