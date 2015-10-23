752d1a199d372c91fd2ce5489e46a3fabeea03ad
Emerson Farrugia <emerson@openmhealth.org>
2015-10-23 14:58:35 +0200

    Rewrite instance configuration to match GitHub samples

--- resources/google-compute-engine/vm_template.py
+++ resources/google-compute-engine/vm_template.py
@@ -10,55 +10,63 @@
 
 import yaml
 
+COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
 
-def GenerateEmbeddableYaml(yaml_string):
-    # Because YAML is a space delimited format, we need to be careful about
-    # embedding one YAML document in another. This function takes in a string in
-    # YAML format and produces an equivalent YAML representation that can be
-    # inserted into arbitrary points of another YAML document. It does so by
-    # printing the YAML string in a single line format. Consistent ordering of
-    # the string is also guaranteed by using yaml.dump.
-    yaml_object = yaml.load(yaml_string)
-    dumped_yaml = yaml.dump(yaml_object, default_flow_style=True)
-    return dumped_yaml
+
+def GlobalComputeUrl(project, collection, name):
+    return ''.join([COMPUTE_URL_BASE, 'projects/', project, '/global/', collection, '/', name])
+
+
+def ZonalComputeUrl(project, zone, collection, name):
+    return ''.join([COMPUTE_URL_BASE, 'projects/', project, '/zones/', zone, '/', collection, '/', name])
 
 
 def GenerateConfig(context):
-    # We need to specify the container manifest as a YAML string in the instance's
-    # metadata. Generate a YAML string that can be inserted into the overall
-    # document.
-    manifest = GenerateEmbeddableYaml(
-        context.imports[context.properties["containerManifest"]])
+    """Generate configuration."""
 
-    return """
-resources:
-  - type: compute.v1.instance
-    name: %(instanceName)s
-    properties:
-      zone: %(instanceZone)s
-      machineType: zones/%(instanceZone)s/machineTypes/%(instanceMachineType)s
-      metadata:
-        items:
-          - key: google-container-manifest
-            value: "%(manifest)s"
-      disks:
-        - deviceName: boot
-          type: PERSISTENT
-          diskType: %(instanceBootDiskType)s
-          diskSizeGb: 10
-          boot: true
-          autoDelete: true
-          initializeParams:
-            sourceImage: projects/google-containers/global/images/%(instanceImage)s
-      networkInterfaces:
-        - network: global/networks/%(instanceNetwork)s
-          accessConfigs:
-            - name: External NAT
-              type: ONE_TO_ONE_NAT
-""" % {"instanceName": context.properties["instanceName"],
-       "instanceImage": context.properties["instanceImage"],
-       "instanceNetwork": context.properties["instanceNetwork"],
-       "instanceZone": context.properties["instanceZone"],
-       "instanceBootDiskType": context.properties["instanceBootDiskType"],
-       "instanceMachineType": context.properties["instanceMachineType"],
-       "manifest": manifest}
+    # Properties for the container-based instance.
+    instance = {
+        'zone': context.properties['instanceZone'],
+        'machineType': ZonalComputeUrl(context.env['project'],
+                                       context.properties['instanceZone'],
+                                       'machineTypes',
+                                       context.properties['instanceMachineType']),
+        'metadata': {
+            'items': [{
+                'key': 'google-container-manifest',
+                'value': context.imports[context.properties['containerManifest']]
+            }]
+        },
+        'disks': [{
+            'deviceName': 'boot',
+            'type': 'PERSISTENT',
+            'autoDelete': True,
+            'boot': True,
+            'diskSizeInGb': 10,
+            'diskType': context.properties['instanceBootDiskType'],
+            'initializeParams': {
+                'diskName': context.properties['instanceName'] + '-disk',
+                'sourceImage': GlobalComputeUrl('google-containers',
+                                                'images',
+                                                context.properties['instanceImage'])
+            },
+        }],
+        'networkInterfaces': [{
+            'accessConfigs': [{
+                'name': 'external-nat',
+                'type': 'ONE_TO_ONE_NAT'
+            }],
+            'network': GlobalComputeUrl(context.env['project'], 'networks', context.properties['instanceNetwork'])
+        }]
+    }
+
+    # Resources to return.
+    resources = {
+        'resources': [{
+            'name': context.properties['instanceName'],
+            'type': 'compute.v1.instance',
+            'properties': instance
+        }]
+    }
+
+    return yaml.dump(resources)
