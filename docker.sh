8d8804f1c2094b0b4f368152a30d613d369a5744
Emerson Farrugia <emerson.farrugia@gmail.com>
2015-12-03 14:55:52 +0100

    Delete legacy Docker build script

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-BASEDIR=`pwd`
-BUILD_VERSION='0.2.2'
-
-docker build -t "openmhealth/omh-shim-server:$BUILD_VERSION" docker/binary
-docker build -t "openmhealth/omh-shim-server:latest" docker/binary
