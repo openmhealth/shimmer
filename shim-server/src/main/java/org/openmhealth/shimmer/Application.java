ea8a68d1ce6e43bee6d4ba20733346f460dee6c6
Wallace Wadge <wwadge@gmail.com>
2017-03-17 17:53:56 +0100

    Upgrade Spring Boot and Gradle versions

--- shim-server/src/main/java/org/openmhealth/shimmer/Application.java
+++ shim-server/src/main/java/org/openmhealth/shimmer/Application.java
@@ -21,7 +21,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
