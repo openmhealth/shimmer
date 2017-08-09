7e6e3e06db8f07e315934e5a56c5db1579b0bff2
Emerson Farrugia <emerson@openmhealth.org>
2017-08-09 20:20:26 +0100

    Add base classes for OAuth client settings

---
+++ shim-server/src/main/java/org/openmhealth/shim/OAuth2ClientSettings.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.openmhealth.shim;
+
+
+import java.util.List;
+
+
+/**
+ * @author Emerson Farrugia
+ */
+public abstract class OAuth2ClientSettings {
+
+    private String clientId;
+    private String clientSecret;
+
+
+    public String getClientId() {
+
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+
+        this.clientId = clientId;
+    }
+
+    public boolean hasClientId() {
+
+        return clientId != null && !clientId.isEmpty();
+    }
+
+    public String getClientSecret() {
+
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+
+        this.clientSecret = clientSecret;
+    }
+
+    public abstract List<String> getScopes();
+}
