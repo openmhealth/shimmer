3ddbc06fd42333524563d7a5fdd1bb5e341bbcfe
Emerson Farrugia <emerson@openmhealth.org>
2017-09-29 10:51:47 +0100

    Fix Moves physical activity unit tests

--- shim-server/src/test/java/org/openmhealth/shim/moves/mapper/MovesPhysicalActivityDataPointMapperUnitTests.java
+++ shim-server/src/test/java/org/openmhealth/shim/moves/mapper/MovesPhysicalActivityDataPointMapperUnitTests.java
@@ -1,99 +1,100 @@
 package org.openmhealth.shim.moves.mapper;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.hamcrest.Matchers;
 import org.openmhealth.schema.domain.omh.*;
 import org.openmhealth.shim.common.mapper.DataPointMapperUnitTests;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static java.time.ZoneOffset.UTC;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
-import static org.openmhealth.schema.domain.omh.DurationUnit.SECOND;
-import static org.openmhealth.schema.domain.omh.LengthUnit.METER;
-
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.openmhealth.schema.domain.omh.KcalUnit.KILOCALORIE;
+import static org.openmhealth.schema.domain.omh.LengthUnit.METER;
+import static org.openmhealth.schema.domain.omh.TimeInterval.ofStartDateTimeAndEndDateTime;
+import static org.openmhealth.shim.moves.mapper.MovesDataPointMapper.RESOURCE_API_SOURCE_NAME;
+
 
 /**
  * @author Jared Sieling
+ * @author Emerson Farrugia
  */
 public class MovesPhysicalActivityDataPointMapperUnitTests extends DataPointMapperUnitTests {
 
     private final MovesPhysicalActivityDataPointMapper mapper = new MovesPhysicalActivityDataPointMapper();
-    private JsonNode responseNode;
+    private JsonNode storylineResponseNode;
+    private JsonNode storylineNoSegmentsResponseNode;
 
     @BeforeTest
-    public void initializeResponseNode() {
-        responseNode = asJsonNode("org/openmhealth/shim/moves/mapper/moves-storyline.json");
-    }
+    public void initializeResponseNode() throws IOException {
 
-    @Test
-    public void asDataPointsShouldReturnEmptyListWhenResponseIsEmpty() throws IOException {
-        JsonNode emptyResponseNode = asJsonNode("org/openmhealth/shim/moves/mapper/moves-storyline-empty.json");
-        assertThat(mapper.asDataPoints(emptyResponseNode), is(empty()));
+        storylineResponseNode = asJsonNode("/org/openmhealth/shim/moves/mapper/moves-user-storyline-daily.json");
+        storylineNoSegmentsResponseNode =
+                asJsonNode("/org/openmhealth/shim/moves/mapper/moves-user-storyline-daily-no-segments.json");
     }
 
     @Test
     public void asDataPointsShouldReturnCorrectNumberOfDataPoints() {
-        assertThat(mapper.asDataPoints(responseNode).size(), equalTo(8));
+
+        List<DataPoint<PhysicalActivity>> dataPoints = mapper.asDataPoints(storylineResponseNode);
+
+        assertThat(dataPoints, notNullValue());
+        assertThat(dataPoints.size(), equalTo(7)); // activities without time frames are ignored
+    }
+
+    @Test
+    public void asDataPointsShouldReturnEmptyListIfResponseHasNoSegments() throws IOException {
+
+        assertThat(mapper.asDataPoints(storylineNoSegmentsResponseNode), empty());
     }
 
     @Test
     public void asDataPointsShouldReturnCorrectDataPoints() {
-        List<DataPoint<PhysicalActivity>> dataPoints = mapper.asDataPoints(responseNode);
 
-        assertThatDataPointMatches(dataPoints.get(0), "cycling", "2016-04-03", "17:52:00", 1500D, 309L);
-        assertThatDataPointMatches(dataPoints.get(2), "walking", "2016-04-04", "17:54:51", 5844D, 3820L);
+        List<DataPoint<PhysicalActivity>> dataPoints = mapper.asDataPoints(storylineResponseNode);
+
+        assertThatDataPointMatches(dataPoints.get(0),
+                "walking",
+                OffsetDateTime.parse("2012-12-12T07:14:30+02:00"),
+                OffsetDateTime.parse("2012-12-12T07:27:32+02:00"),
+                1251L,
+                99L);
     }
 
-    public void assertThatDataPointMatches(DataPoint<PhysicalActivity> dataPoint, String expectedActivityName,
-                                           TimeFrame expectedTimeFrame, Double expectedDistance) {
+    public void assertThatDataPointMatches(
+            DataPoint<PhysicalActivity> dataPoint,
+            String expectedActivityName,
+            OffsetDateTime expectedEffectiveStartDateTime,
+            OffsetDateTime expectedEffectiveEndDateTime,
+            Long expectedDistanceInM,
+            Long expectedCaloriesInKCal) {
 
         PhysicalActivity.Builder expectedMeasureBuilder = new PhysicalActivity.Builder(expectedActivityName);
 
-        expectedMeasureBuilder.setEffectiveTimeFrame(expectedTimeFrame);
+        expectedMeasureBuilder.setEffectiveTimeFrame(
+                ofStartDateTimeAndEndDateTime(expectedEffectiveStartDateTime, expectedEffectiveEndDateTime));
 
-        if (expectedDistance != null) {
-            expectedMeasureBuilder.setDistance(new LengthUnitValue(METER, expectedDistance));
+        if (expectedDistanceInM != null) {
+            expectedMeasureBuilder.setDistance(new LengthUnitValue(METER, expectedDistanceInM));
+        }
+
+        if (expectedCaloriesInKCal != null) {
+            expectedMeasureBuilder.setCaloriesBurned(new KcalUnitValue(KILOCALORIE, expectedCaloriesInKCal));
         }
 
         PhysicalActivity expectedPhysicalActivity = expectedMeasureBuilder.build();
 
-        assertThat(dataPoint.getBody(), Matchers.equalTo(expectedPhysicalActivity));
-        assertThat(dataPoint.getHeader().getBodySchemaId(), Matchers.equalTo(PhysicalActivity.SCHEMA_ID));
+        assertThat(dataPoint.getHeader().getBodySchemaId(), equalTo(PhysicalActivity.SCHEMA_ID));
+        assertThat(dataPoint.getBody(), equalTo(expectedPhysicalActivity));
 
-        assertThat(dataPoint.getHeader().getAcquisitionProvenance().getSourceName(),
-                Matchers.equalTo(MovesDataPointMapper.RESOURCE_API_SOURCE_NAME));
+        DataPointAcquisitionProvenance acquisitionProvenance = dataPoint.getHeader().getAcquisitionProvenance();
+
+        assertThat(acquisitionProvenance, notNullValue());
+        assertThat(acquisitionProvenance.getSourceName(), equalTo(RESOURCE_API_SOURCE_NAME));
     }
-
-    public void assertThatDataPointMatches(DataPoint<PhysicalActivity> dataPoint, String expectedActivityName,
-                                           String expectedEffectiveDate, String expectedEffectiveStartTime, Double expectedDistance,
-                                           Long expectedDurationInMs) {
-
-        OffsetDateTime expectedEffectiveStartDateTime = OffsetDateTime
-                .of(LocalDate.parse(expectedEffectiveDate), LocalTime.parse(expectedEffectiveStartTime), ZoneOffset.ofHours(-5));
-
-        TimeFrame expectedTimeFrame;
-
-        if (expectedDurationInMs != null) {
-            DurationUnitValue expectedDuration = new DurationUnitValue(SECOND, expectedDurationInMs);
-
-            expectedTimeFrame = new TimeFrame(
-                    TimeInterval.ofStartDateTimeAndDuration(expectedEffectiveStartDateTime, expectedDuration));
-        }
-        else {
-            expectedTimeFrame = new TimeFrame(expectedEffectiveStartDateTime);
-        }
-
-        assertThatDataPointMatches(dataPoint, expectedActivityName, expectedTimeFrame, expectedDistance);
-    }
-
 }
