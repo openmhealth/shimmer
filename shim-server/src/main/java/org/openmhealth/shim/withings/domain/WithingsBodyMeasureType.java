eddcbcc390dd0acd7cd78c5c29f6d8c437a62084
Emerson Farrugia <emerson@openmhealth.org>
2017-10-01 21:05:37 +0100

    Add Withings body temperature measure type

--- shim-server/src/main/java/org/openmhealth/shim/withings/domain/WithingsBodyMeasureType.java
+++ shim-server/src/main/java/org/openmhealth/shim/withings/domain/WithingsBodyMeasureType.java
@@ -30,13 +30,11 @@ public enum WithingsBodyMeasureType {
 
     BODY_WEIGHT(1),
     BODY_HEIGHT(4),
-    // FAT_FREE_MASS(5), // TODO confirm what this means
-    // FAT_RATIO(6), // TODO confirm what this means
-    // FAT_MASS_WEIGHT(8), // TODO confirm what this means
     DIASTOLIC_BLOOD_PRESSURE(9),
     SYSTOLIC_BLOOD_PRESSURE(10),
     HEART_RATE(11),
-    OXYGEN_SATURATION(54);
+    OXYGEN_SATURATION(54),
+    BODY_TEMPERATURE(71);
 
     private int magicNumber;
 
