6a3ff578abf2f578f59eff8db76785bb78a12458
Emerson Farrugia <emerson@openmhealth.org>
2017-10-01 16:59:27 +0100

    Update Misfit mappers to use SleepDuration2

--- shim-server/src/main/java/org/openmhealth/shim/misfit/mapper/MisfitSleepDurationDataPointMapper.java
+++ shim-server/src/main/java/org/openmhealth/shim/misfit/mapper/MisfitSleepDurationDataPointMapper.java
@@ -19,32 +19,31 @@ package org.openmhealth.shim.misfit.mapper;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.openmhealth.schema.domain.omh.DataPoint;
 import org.openmhealth.schema.domain.omh.DurationUnitValue;
-import org.openmhealth.schema.domain.omh.SleepDuration1;
-import org.openmhealth.shim.common.mapper.JsonNodeMappingException;
+import org.openmhealth.schema.domain.omh.SleepDuration2;
+import org.openmhealth.schema.domain.omh.SleepEpisode;
 
-import java.time.Duration;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 
-import static java.lang.String.format;
+import static java.util.Optional.empty;
 import static org.openmhealth.schema.domain.omh.DurationUnit.SECOND;
 import static org.openmhealth.schema.domain.omh.TimeInterval.ofStartDateTimeAndEndDateTime;
-import static org.openmhealth.shim.common.mapper.JsonNodeMappingSupport.*;
+import static org.openmhealth.shim.common.mapper.JsonNodeMappingSupport.asOptionalBoolean;
+import static org.openmhealth.shim.common.mapper.JsonNodeMappingSupport.asOptionalString;
+import static org.openmhealth.shim.misfit.mapper.MisfitSleepMeasureDataPointMapper.SleepSegmentType.AWAKE;
 
 
 /**
- * A mapper from Misfit Resource API /activity/sleeps responses to {@link SleepDuration1} objects. This mapper
- * currently creates a single data point per sleep node in the response, subtracting the duration of awake segments
- * from the sleep duration. It's also possible to create a single data point per sleep segment, which would help
- * preserve the granularity of the original data. This mapper may be updated to return a data point per segment in the
- * future.
+ * A mapper from Misfit Resource API /activity/sleeps responses to {@link SleepDuration2} objects. This mapper currently
+ * creates a single data point per sleep node in the response, subtracting the duration of awake segments from the sleep
+ * duration. It's also possible to create a single data point per sleep segment, which would help preserve the
+ * granularity of the original data, but that may be better suited to {@link SleepEpisode} measures.
  *
  * @author Emerson Farrugia
- * @see <a href="https://build.misfit.com/docs/references#APIReferences-Sleep">API documentation</a>
+ * @see <a href="https://build.misfit.com/docs/cloudapi/api_references#sleep">API documentation</a>
  */
-public class MisfitSleepDurationDataPointMapper extends MisfitDataPointMapper<SleepDuration1> {
-
-    public static final int AWAKE_SEGMENT_TYPE = 1;
+public class MisfitSleepDurationDataPointMapper extends MisfitSleepMeasureDataPointMapper<SleepDuration2> {
 
     @Override
     protected String getListNodeName() {
@@ -52,55 +51,30 @@ public class MisfitSleepDurationDataPointMapper extends MisfitDataPointMapper<Sl
     }
 
     @Override
-    public Optional<DataPoint<SleepDuration1>> asDataPoint(JsonNode sleepNode) {
+    public Optional<DataPoint<SleepDuration2>> asDataPoint(JsonNode sleepNode) {
 
-        // The sleep details array contains segments corresponding to whether the user was awake, sleeping lightly,
-        // or sleeping restfully for the duration of that segment. To discount the awake segments, we have to deduct
-        // their duration from the total sleep duration.
-        JsonNode sleepDetailsNode = asRequiredNode(sleepNode, "sleepDetails");
+        List<SleepSegment> sleepSegments = asSleepSegments(sleepNode);
 
-        long awakeDurationInSec = 0;
+        Optional<OffsetDateTime> effectiveStartDateTime = getSleepOnsetDateTime(sleepSegments);
 
-        OffsetDateTime previousSegmentStartDateTime = null;
-        Long previousSegmentType = null;
-
-        for (JsonNode sleepDetailSegmentNode : sleepDetailsNode) {
-
-            OffsetDateTime startDateTime = asRequiredOffsetDateTime(sleepDetailSegmentNode, "datetime");
-            Long value = asRequiredLong(sleepDetailSegmentNode, "value");
-
-            // if the user was awake, add it to the awake tally
-            if (previousSegmentType != null && previousSegmentType == AWAKE_SEGMENT_TYPE) {
-                awakeDurationInSec += Duration.between(previousSegmentStartDateTime, startDateTime).getSeconds();
-            }
-
-            previousSegmentStartDateTime = startDateTime;
-            previousSegmentType = value;
+        if (!effectiveStartDateTime.isPresent()) {
+            return empty();
         }
 
-        // checking if the segment array is empty this way avoids compiler confusion later
-        if (previousSegmentType == null) {
-            throw new JsonNodeMappingException(format("The Misfit sleep node '%s' has no sleep details.", sleepNode));
-        }
+        OffsetDateTime effectiveEndDateTime = getArisingDateTime(sleepSegments)
+                .orElseThrow(IllegalStateException::new);
 
-        // to calculate the duration of last segment, first determine the overall end time
-        OffsetDateTime startDateTime = asRequiredOffsetDateTime(sleepNode, "startTime");
-        Long totalDurationInSec = asRequiredLong(sleepNode, "duration");
-        OffsetDateTime endDateTime = startDateTime.plusSeconds(totalDurationInSec);
+        long sleepDurationInSec = sleepSegments.stream()
+                .filter((segment) -> segment.getType() != AWAKE)
+                .mapToLong(SleepSegment::getDurationInSec)
+                .sum();
 
-        if (previousSegmentType == AWAKE_SEGMENT_TYPE) {
-            awakeDurationInSec += Duration.between(previousSegmentStartDateTime, endDateTime).getSeconds();
-        }
-
-        Long sleepDurationInSec = totalDurationInSec - awakeDurationInSec;
-
-        if (sleepDurationInSec == 0) {
-            return Optional.empty();
-        }
-
-        SleepDuration1 measure = new SleepDuration1.Builder(new DurationUnitValue(SECOND, sleepDurationInSec))
-                .setEffectiveTimeFrame(ofStartDateTimeAndEndDateTime(startDateTime, endDateTime))
-                .build();
+        SleepDuration2 measure =
+                new SleepDuration2.Builder(
+                        new DurationUnitValue(SECOND, sleepDurationInSec),
+                        ofStartDateTimeAndEndDateTime(effectiveStartDateTime.get(), effectiveEndDateTime)
+                )
+                        .build();
 
         String externalId = asOptionalString(sleepNode, "id").orElse(null);
         Boolean sensed = asOptionalBoolean(sleepNode, "autoDetected").orElse(null);
